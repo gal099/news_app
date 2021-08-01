a3ed56c5770b94f8fb8c7223f080fb82a41e98c3
Juan <jb@hotmail.com>
2021-07-31 21:39:38 -0300

    agrego fetch

--- src/components/MainNews.jsx
+++ src/components/MainNews.jsx
@@ -1,18 +1,28 @@
-import data from '../data/data.json';
+import { useEffect, useState } from 'react';
+
 import styles from './MainNews.module.css';
 
 export function MainNews() {
-  const { articles } = data;
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/top-headlines?country=ar&category=business&pageSize=1&apiKey=e0c33b0d8e844a1cb0b348063672668d'
+    )
+      .then((result) => result.json())
+      .then((data) => setNews(data.articles[0]));
+  }, []);
+
+  if (!news) {
+    return null;
+  }
+
   return (
     <div className={styles.mainNewsContainer}>
-      <img
-        className={styles.mainImg}
-        src={articles[0].urlToImage}
-        alt={articles[0].title}
-      />
+      <img className={styles.mainImg} src={news.urlToImage} alt={news.title} />
       <div>
-        <h2 className={styles.mainTitle}>{articles[0].title}</h2>
-        <p className={styles.mainDescription}>{articles[0].description} </p>
+        <h2 className={styles.mainTitle}>{news.title}</h2>
+        <p className={styles.mainDescription}>{news.description} </p>
       </div>
     </div>
   );
