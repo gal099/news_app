73cdecb125783f9aaef61efb8e4207e2735dffd4
Juan <jb@hotmail.com>
2021-07-31 21:39:49 -0300

    agrego fetch

--- src/components/NewsGrid.jsx
+++ src/components/NewsGrid.jsx
@@ -1,11 +1,25 @@
-import data from '../data/data.json';
+import { useEffect, useState } from 'react';
+
 import styles from './NewsGrid.module.css';
 
 export function NewsGrid() {
-  const { articles } = data;
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/top-headlines?country=ar&apiKey=e0c33b0d8e844a1cb0b348063672668d'
+    )
+      .then((result) => result.json())
+      .then((data) => setNews(data.articles));
+  }, []);
+
+  if (!news) {
+    return null;
+  }
+
   return (
     <ul className={styles.newsGridContainer}>
-      {articles.map((elem, i) => (
+      {news.map((elem) => (
         <li key={elem.url} className={styles.newsCard}>
           <img
             className={styles.gridImg}
