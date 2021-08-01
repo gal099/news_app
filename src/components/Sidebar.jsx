87413997d9a4b6985b17a38f2ef55944e5a08c43
Juan <jb@hotmail.com>
2021-07-31 21:39:54 -0300

    agrego fetch

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,33 @@
-import data from '../data/data.json';
+import { useEffect, useState } from 'react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
-  const { articles } = data;
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/top-headlines?country=ar&category=sports&pageSize=5&apiKey=e0c33b0d8e844a1cb0b348063672668d'
+    )
+      .then((result) => result.json())
+      .then((data) => setNews(data.articles));
+  }, []);
+
+  if (!news) {
+    return null;
+  }
+
   return (
     <ul className={styles.newsContainer}>
-      {articles.map((elem, i) => (
+      <h3 className={styles.newsContainerTitle}>DEPORTES</h3>
+      {news.map((elem, i) => (
         <li key={elem.url} className={styles.newsCard}>
           <img
-            className={styles.gridImg}
+            className={styles.newsCardImg}
             src={elem.urlToImage}
             alt={elem.title}
           />
-          <div className={styles.gridTitle}>{elem.title}</div>
+          <div className={styles.newsCardTitle}>{elem.title}</div>
         </li>
       ))}
     </ul>
